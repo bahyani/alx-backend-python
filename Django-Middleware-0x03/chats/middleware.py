79afed456c7a7e38e4a6895d88b97cb6b291cd22
bahyani <ikennaiwu@yahoo.com>
2025-12-02 16:46:46 +0100

    Add RequestLoggingMiddleware and configure settings

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -3,6 +3,7 @@ import logging
 from datetime import datetime
 from django.conf import settings
 from pathlib import Path
+from django.http import HttpResponseForbidden
 
 # Ensure logs go to project root (BASE_DIR)
 LOG_PATH = Path(settings.BASE_DIR) / "requests.log"
@@ -28,6 +29,26 @@ class RequestLoggingMiddleware:
         log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
 
         logging.info(log_message)
+
+        response = self.get_response(request)
+        return response
+
+
+class RestrictAccessByTimeMiddleware:
+    """
+    Denies access to chat endpoints outside 6 AM - 9 PM
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Only restrict the chat endpoints
+        if request.path.startswith("/api/messages/"):
+            current_hour = datetime.now().hour
+            # Deny access before 6AM or after 9PM (21)
+            if current_hour < 6 or current_hour >= 21:
+                return HttpResponseForbidden("Chat access is allowed only between 6AM and 9PM")
 
         response = self.get_response(request)
         return response
