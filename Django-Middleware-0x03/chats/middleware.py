4c3693e28e2bb57c4755902578437dac5e07923a
bahyani <ikennaiwu@yahoo.com>
2025-12-02 16:33:03 +0100

    Add RequestLoggingMiddleware and configure settings

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,18 @@
 
 import logging
 from datetime import datetime
+from django.conf import settings
+from pathlib import Path
+
+# Ensure logs go to project root (BASE_DIR)
+LOG_PATH = Path(settings.BASE_DIR) / "requests.log"
+
+# Configure logging once (module import)
+logging.basicConfig(
+    filename=str(LOG_PATH),
+    level=logging.INFO,
+    format="%(message)s"
+)
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
